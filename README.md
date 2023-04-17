74fa76e369dc52bb2226dc419d0694f6bab4d635
ZhangQingQiuA <130888251+ZhangQingQiuA@users.noreply.github.com>
2023-04-17 23:14:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# OpenWRT-CI
+云编译OpenWRT固件
+
+LEDE源码(LWRT)：
+https://github.com/coolsnowwolf/lede
+
+IMMORTALWRT源码(IWRT)：
+https://github.com/immortalwrt/immortalwrt
+
+# 目录及文件简要说明：
+
+Depends.txt——环境依赖列表
+
+Scripts——自定义脚本
+
+Config——自定义配置
+
+workflows——自定义CI配置
---
+++ Scripts/Plugins.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Argon Theme
+git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-theme-argon.git
+#Argon Config
+git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
+#Design Theme
+git clone --depth=1 --single-branch https://github.com/gngpp/luci-theme-design.git
+#Design Config
+git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
+#Open Clash
+git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
+
+#Open Clash Core
+export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
+export CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux
+export CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux
+export CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux
+
+export CORE_TYPE=$(if [[ $OWRT_TYPE == *"64"* ]] ; then echo "amd64" ; else echo "arm64" ; fi)
+export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
+
+cd ./OpenClash/luci-app-openclash/root/etc/openclash
+mkdir ./core && cd ./core
+
+curl -sfL -o ./tun.gz "$CORE_TUN"-"$CORE_TYPE"-"$TUN_VER".gz
+gzip -d ./tun.gz && mv ./tun ./clash_tun
+
+curl -sfL -o ./meta.tar.gz "$CORE_MATE"-"$CORE_TYPE".tar.gz
+tar -zxf ./meta.tar.gz && mv ./clash ./clash_meta
+
+curl -sfL -o ./dev.tar.gz "$CORE_DEV"-"$CORE_TYPE".tar.gz
+tar -zxf ./dev.tar.gz
+
+chmod +x ./clash* ; rm -rf ./*.gz
